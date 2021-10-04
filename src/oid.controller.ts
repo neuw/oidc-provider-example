63f1c6f6e4c0bacc7751d9068c73efe6dbd74361
Karanbir Singh <krnbr@live.in>
2021-10-04 22:22:11 +0530

    changes to cover a specific scenario

--- src/oid.controller.ts
+++ src/oid.controller.ts
@@ -3,19 +3,28 @@ import { Request, Response } from "express";
 import { Configuration, Provider } from "oidc-provider";
 
 // http://localhost:3000/oidc/auth?client_id=test&response_type=code&scope=openid&redirect_uri=http://localhost:53538/oidc/callback
+// http://localhost:3000/oidc/auth?client_id=test&response_type=code&scope=openid%20offline_access&redirect_uri=http://localhost:53538/oidc/callback&prompt=consent
+// http://localhost:3000/oidc/auth?client_id=test&response_type=code&scope=openid%20offline_access&redirect_uri=http://localhost:53538/oidc/callback&grant_type=refresh_token
 const configuration: Configuration = {
   clients: [
     {
       client_id: "test",
       client_secret: "test",
-      grant_types: ["client_credentials", "authorization_code"],
+      grant_types: [
+        "refresh_token",
+        "client_credentials",
+        "authorization_code",
+      ],
       redirect_uris: ["http://localhost:53538/oidc/callback"],
       response_types: ["code"],
       token_endpoint_auth_method: "none",
+      scope: "offline_access openid",
+      application_type: "web",
     },
   ],
   ttl: {
     ClientCredentials: 3600,
+    RefreshToken: 3600 * 24 * 30,
   },
   features: {
     clientCredentials: {
@@ -37,8 +46,11 @@ const configuration: Configuration = {
       enabled: true,
       getResourceServerInfo: (ctx, resourceIndicator, client) => {
         console.log("resource indicator: ", resourceIndicator, client);
+        /*console.log("ctx: ", ctx);
+        console.log("ctx req: ", ctx.req);
+        console.log("ctx res: ", ctx.res);*/
         return {
-          scope: "api:read api:write",
+          scope: "openid offline_access api:read api:write",
           audience: "resource-server-audience-value",
           accessTokenTTL: 2 * 60 * 60, // 2 hours
           accessTokenFormat: "jwt",
