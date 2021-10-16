cdddda76c74b517c55855026c19daf17d3cf4761
Karanbir Singh <krnbr@live.in>
2021-10-16 14:42:59 +0530

    minor change

--- src/oid.controller.ts
+++ src/oid.controller.ts
@@ -26,6 +26,31 @@ const configuration: Configuration = {
     ClientCredentials: 3600,
     RefreshToken: 3600 * 24 * 30,
   },
+  loadExistingGrant: async (ctx) => {
+    const grantId =
+      (ctx.oidc.result &&
+        ctx.oidc.result.consent &&
+        ctx.oidc.result.consent.grantId) ||
+      ctx.oidc.session.grantIdFor(ctx.oidc.client.clientId);
+
+    if (grantId) {
+      return ctx.oidc.provider.Grant.find(grantId);
+    } else if (isFirstParty(ctx.oidc.client)) {
+      const grant = new ctx.oidc.provider.Grant({
+        clientId: ctx.oidc.client.clientId,
+        accountId: ctx.oidc.session.accountId,
+      });
+
+      grant.addOIDCScope("openid email profile");
+      grant.addOIDCClaims(["first_name"]);
+      grant.addResourceScope(
+        "urn:example:resource-indicator",
+        "api:read api:write"
+      );
+      await grant.save();
+      return grant;
+    }
+  },
   features: {
     clientCredentials: {
       enabled: true,
@@ -77,6 +102,15 @@ const configuration: Configuration = {
     methods: ["plain"],
   },
 };
+
+function isFirstParty(ctx) {
+  console.log("isFirstParty, ", ctx);
+  console.log("ctx.Client ", ctx.Client);
+  if (ctx.clientId === "test") {
+    return true;
+  }
+  return false;
+}
 
 const oidc = new Provider("http://localhost:3000", configuration);
 const callback = oidc.callback();
