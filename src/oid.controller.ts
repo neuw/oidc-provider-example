e6b0e70a2a109f6a2b050eae41fa26046314e1d8
Karanbir Singh <karanbir.singh@team.telstra.com>
2021-12-12 08:48:17 +0530

    minor changes, multiple changes

--- src/oid.controller.ts
+++ src/oid.controller.ts
@@ -1,31 +1,30 @@
 import { All, Controller, Req, Res } from "@nestjs/common";
 import { Request, Response } from "express";
-import { Configuration, Provider } from "oidc-provider";
+import { Configuration, interactionPolicy, Provider } from "oidc-provider";
 
-// http://localhost:3000/oidc/auth?client_id=test&response_type=code&scope=openid&redirect_uri=http://localhost:53538/oidc/callback
-// http://localhost:3000/oidc/auth?client_id=test&response_type=code&scope=openid%20offline_access&redirect_uri=http://localhost:53538/oidc/callback&prompt=consent
-// http://localhost:3000/oidc/auth?client_id=test&response_type=code&scope=openid%20offline_access&redirect_uri=http://localhost:53538/oidc/callback&grant_type=refresh_token
 const configuration: Configuration = {
   clients: [
     {
       client_id: "test",
       client_secret: "test",
-      grant_types: [
-        "refresh_token",
-        "client_credentials",
-        "authorization_code",
-      ],
+      grant_types: ["client_credentials", "authorization_code"],
       redirect_uris: ["http://localhost:53538/oidc/callback"],
       response_types: ["code"],
       token_endpoint_auth_method: "none",
-      scope: "offline_access openid",
+      scope: "openid",
       application_type: "web",
     },
   ],
   ttl: {
-    ClientCredentials: 3600,
+    ClientCredentials: 840,
     RefreshToken: 3600 * 24 * 30,
+    AccessToken: 840,
+    IdToken: 840,
+    Session: 840,
   },
+  /*issueRefreshToken: async (ctx) => {
+    return true;
+  },*/
   loadExistingGrant: async (ctx) => {
     const grantId =
       (ctx.oidc.result &&
@@ -34,22 +33,21 @@ const configuration: Configuration = {
       ctx.oidc.session.grantIdFor(ctx.oidc.client.clientId);
 
     if (grantId) {
+      /*console.log("not in isFirstParty");*/
       return ctx.oidc.provider.Grant.find(grantId);
     } else if (isFirstParty(ctx.oidc.client)) {
+      /*console.log("in isFirstParty", ctx);*/
       const grant = new ctx.oidc.provider.Grant({
         clientId: ctx.oidc.client.clientId,
         accountId: ctx.oidc.session.accountId,
       });
 
-      grant.addOIDCScope("openid email profile");
-      grant.addOIDCClaims(["first_name"]);
-      grant.addResourceScope(
-        "urn:example:resource-indicator",
-        "api:read api:write"
-      );
+      grant.addOIDCScope("openid email profile offline_access");
+      grant.addOIDCClaims([]);
       await grant.save();
       return grant;
     }
+    return undefined;
   },
   features: {
     clientCredentials: {
@@ -64,32 +62,10 @@ const configuration: Configuration = {
     jwtIntrospection: {
       enabled: true,
     },
-    resourceIndicators: {
-      defaultResource: (ctx) => {
-        return "http://example.com";
-      },
-      enabled: true,
-      getResourceServerInfo: (ctx, resourceIndicator, client) => {
-        console.log("resource indicator: ", resourceIndicator, client);
-        /*console.log("ctx: ", ctx);
-        console.log("ctx req: ", ctx.req);
-        console.log("ctx res: ", ctx.res);*/
-        return {
-          scope: "openid offline_access api:read api:write",
-          audience: "resource-server-audience-value",
-          accessTokenTTL: 2 * 60 * 60, // 2 hours
-          accessTokenFormat: "jwt",
-          jwt: {
-            sign: { alg: "RS256" },
-          },
-        };
-      },
-    },
   },
   formats: {
     customizers: {
       async jwt(ctx, token, jwt) {
-        //jwt.header = { foo: "bar" };
         jwt.payload.foo = "bar";
         return jwt;
       },
@@ -99,20 +75,33 @@ const configuration: Configuration = {
     required: (ctx, client) => {
       return false;
     },
-    methods: ["plain"],
+    methods: ["plain", "S256"],
+  },
+  findAccount: async (ctx, sub, token) => {
+    console.log("findAccount ctx", ctx);
+    console.log("findAccount sub", sub);
+    console.log("findAccount token", token);
+    return {
+      accountId: sub,
+      async claims(use, scope, claims, rejected) {
+        return { sub };
+      },
+    };
   },
 };
 
 function isFirstParty(ctx) {
-  console.log("isFirstParty, ", ctx);
-  console.log("ctx.Client ", ctx.Client);
   if (ctx.clientId === "test") {
     return true;
   }
   return false;
 }
 
-const oidc = new Provider("http://localhost:3000", configuration);
+const oidc = new Provider(
+  "https://example.com",
+  configuration
+);
+oidc.proxy = true;
 const callback = oidc.callback();
 
 function handleClientAuthErrors(
@@ -137,3 +126,6 @@ export class OidcController {
     return callback(req, res);
   }
 }
+
+// http://localhost:35553/oidc/auth?client_id=test&redirect_uri=http://localhost:53538/oidc/callback&response_type=code&scope=openid&state=3aaf1d6817534d7298b56c22005e8eab&code_challenge=123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123&code_challenge_method=plain&response_mode=query
+// https://example.com/oidc/auth?client_id=test&redirect_uri=http://localhost:53538/oidc/callback&response_type=code&scope=openid&state=3aaf1d6817534d7298b56c22005e8eab&code_challenge=123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123&code_challenge_method=plain&response_mode=query
